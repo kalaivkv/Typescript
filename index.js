5c602a6b2be2162045210275628326dbd2b481d8
kalaivanan <kalaivkv24@gmail.com>
2025-03-01 16:38:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/* Variable Types*/
+var username = "kalaivanan";
+var age = 300;
+var isDeveloper = true;
+var skills = ["Javascript", "TypeScript"];
+/* Functions in TypeScript */
+function add(a, b) {
+    return a + b;
+}
+console.log(add(57, 10));
+/*  Arrow function */
+var multiply = function (x, y) { return x * y; };
+console.log(multiply(10, 5));
+var user1 = { id: 1, name: "KalaiVanan", email: "kalaivkv24@gmail.com" };
+/* Classes & Inheritance */
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.greet = function () {
+        console.log("Hello, my name is ".concat(this.name));
+    };
+    return Person;
+}());
+var Developer = /** @class */ (function (_super) {
+    __extends(Developer, _super);
+    function Developer(name, age, skills) {
+        var _this = _super.call(this, name, age) || this;
+        _this.skills = skills;
+        return _this;
+    }
+    return Developer;
+}(Person));
+var dev = new Developer("kalai", 30, ["Javascript", "TypeScript"]);
+dev.greet();
+console.log(dev.skills);
+/* Enums */
+var Role;
+(function (Role) {
+    Role["User"] = "USER";
+    Role["Admin"] = "ADMIN";
+    Role["Guest"] = "GUEST";
+})(Role || (Role = {}));
+var userRole = Role.Admin;
+console.log(userRole);
+/* any , unknown */
+var data;
+data = "Hello";
+data = 42;
+data = true;
+console.log(data);
+var input;
+input = "Hello kalaivanan .";
+console.log(input.toUpperCase());
+input = 100;
+if (typeof input === "number") {
+    console.log(input.toFixed(2));
+}
+function throwError(message) {
+    throw new Error(message);
+}
+// Usage
+throwError("Something went wrong!"); // 🚀 This function never returns
+/* Tuples */
+var user = [1, "kalai"];
+/* Intersection Type */
+var value;
+value = "Hello kalai";
+value = 24;
---
+++ index.ts
@@ -0,0 +1,89 @@
+
+/* Variable Types*/
+let username: string = "kalaivanan";  
+let age: number = 300;           
+let isDeveloper: boolean = true; 
+let skills: string[] = ["Javascript", "TypeScript"]; 
+
+
+/* Functions in TypeScript */
+function add(a: number, b: number): number {
+    return a + b;
+  }
+  console.log(add(57, 10));  
+ 
+  
+/*  Arrow function */
+const multiply = (x: number, y: number): number => x * y;
+console.log(multiply(10, 5)); 
+
+
+/* Interfaces*/
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    isAdmin?: boolean; 
+  }
+  const user1: User = { id: 1, name: "KalaiVanan", email: "kalaivkv24@gmail.com" };
+
+  
+/* Classes & Inheritance */
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet(): void {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+class Developer extends Person {
+  skills: string[];
+
+  constructor(name: string, age: number, skills: string[]) {
+    super(name, age);
+    this.skills = skills;
+  }
+}
+const dev = new Developer("kalai", 30, ["Javascript", "TypeScript"]);
+dev.greet();
+console.log(dev.skills);
+
+/* Enums */
+enum Role {
+    User = "USER",
+    Admin = "ADMIN",
+    Guest = "GUEST",
+  }
+  const userRole: Role = Role.Admin;
+  console.log(userRole); 
+  
+/* any , unknown */
+let data: any;
+data = "Hello";   
+data = 42;        
+data = true;      
+console.log(data); 
+
+let input: unknown;
+input = "Hello kalaivanan .";
+console.log((input as string).toUpperCase()); 
+input = 100;
+if (typeof input === "number") {
+  console.log(input.toFixed(2)); 
+} 
+  
+/* Tuples */
+let user: [number, string] = [1, "kalai"];
+
+/* Intersection Type */
+let value: string | number;
+value = "Hello kalai";  
+value = 24;       
+
+
